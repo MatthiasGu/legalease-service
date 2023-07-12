303e2d7fce5311d2e4c37def85d82c1c08428cd8
Motiejus Gudenas <mj.gudenas@gmail.com>
2023-07-12 10:46:22 +0100

    Add config files

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  extends: ['airbnb-typescript', 'prettier'],
+  rules: {
+    'no-underscore-dangle': 'off',
+  },
+};
